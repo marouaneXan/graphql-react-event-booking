4c4204a0f90fd56abfa4107e6d320c19048111f9
marouane zahaoui <marouanezahaoui835@gmail.com>
2022-07-20 16:16:24 +0100

    create event type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -8,3 +8,12 @@ const {
 }=require('graphql')
 //mongoose model
 const Event = require('../models/Events')
+
+//Event Type
+const EventType=new GraphQLObjectType({
+    name:'Event',
+    fields:()=>({
+        id:{type:GraphQLID},
+        name:{type:GraphQLString}
+    })
+})
